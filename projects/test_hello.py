ec75f4d25e6dd564758f4412f3d536b6452eb179
Nasta2012 <nastaran.seifi.12@gmail.com>
2025-11-12 12:19:27 +0330

    Create test_hello.py

---
+++ projects/test_hello.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  3 11:43:55 2023
+
+@author: nasta
+"""
+
+from hello import hello 
+
+def main():
+    test_hello()
+    
+
+def test_hello():
+    try:
+        assert hello()=="hello, world"
+    except AssertionError:
+        print("wronge")
+    try:
+        assert hello(David)=="hello, David"
+    except AssertionError:
+        print("wronge2")
+        
+    
+if __name__ == "__main__":
+    main()
+    
